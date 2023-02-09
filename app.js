898efe7daf923e11e1e3217c5b53b2167bfd6cb8
Rajat Sadh <46292539+rajatsadh24@users.noreply.github.com>
2023-02-10 01:55:04 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ console.log('Is this working?');
 let viz;
 
 //Add Share Link to Tableau Public in here
-const url = "https://public.tableau.com/views/Squirrels_15746293266160/Dashboard1?:display_count=y&:origin=viz_share_link";
+const url = "https://public.tableau.com/shared/NSY4P8NXT/?:embed=y&:display_count=yes&:origin=viz_share_link";
 
 const vizContainer = document.getElementById('vizContainer');
 const options = {
